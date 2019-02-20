f766afbec14fa61559dc0c3b0ec3c40aab2043b0
Ben Pham <benjamin.pham@gmail.com>
2019-02-20 13:55:38 +0000

    Created Flat model and controller, CRUD routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,36 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Flat.destroy_all
+
+puts 'Creating flats...'
+flats_attributes = [
+  {
+    name: 'Light & Spacious Garden Flat London',
+    address: '10 Clifton Gardens London W9 1DT',
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: 75,
+    number_of_guests: 3
+  },
+  {
+    name: 'Small & Dark Basement Studio Paris',
+    address: '52 avenue de Clichy 75017 Paris',
+    description: 'The flat is small, humid and has not natural light.',
+    price_per_night: 15,
+    number_of_guests: 1
+  },
+  {
+    name: 'Penthouse 5 bedroom flat in London',
+    address: '46 Kay Street London',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus suscipit asperiores, earum. Ducimus odit facere, adipisci eos quibusdam.',
+    price_per_night: 450,
+    number_of_guests: 10
+  },
+  {
+    name: 'Charming 2 bedroom flat in Paris',
+    address: '4 square du Roule 75008 Paris',
+    description: 'A charming 2 bedroom flat in Paris, fully equipped and close to all main points of interest.',
+    price_per_night: 75,
+    number_of_guests: 3
+  }
+]
+Flat.create!(flats_attributes)
+puts 'Finished!'
